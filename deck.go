76b8008e0a12e0a72737b79bf6396c2d4c62f7fe
Marcus Bergo <marcusbergo@gmail.com>
2020-03-26 09:05:21 +0100

    newDeck fun

--- deck.go
+++ deck.go
@@ -5,7 +5,17 @@ import "fmt"
 type deck []string
 
 func newDeck() deck {
+	cards := deck{}
 
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
 }
 
 func (d deck) print() {
